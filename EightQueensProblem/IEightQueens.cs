b4391fed91ff154ba3dbf1fad8d7e930de4497e9
Leonov Artem <leonovru@yandex.ru>
2019-01-25 10:56:56 +0300

    Change IEightQueens.cs

--- EightQueensProblem/IEightQueens.cs
+++ EightQueensProblem/IEightQueens.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace EightQueensProblem
 {
     interface IEightQueens
     {
+        int AmountSolutions();
+        List<List<Point>> Solutions();
     }
 }
